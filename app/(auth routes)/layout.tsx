bf9f479aeff2382c9ace6715ab659b381f48d60f
Yurii Kit <insidelkit@gmail.com>
2025-11-02 19:17:31 +0200

    added auth layout

---
+++ app/(auth routes)/layout.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
+  return <div className="auth-layout">{children}</div>;
+}
